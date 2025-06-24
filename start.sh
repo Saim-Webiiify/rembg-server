6da3d3057640ff42a9c0dc324e5b436d6789ff58
Saim-Webiiify <navedsaim1@gmail.com>
2025-06-24 18:11:44 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-rembg s --host 0.0.0.0 --port $PORT --log-level info
+
+# Use default port if not set
+PORT=${PORT:-10000}
+
+# Run rembg server on the correct port
+exec rembg s --host 0.0.0.0 --port $PORT --log-level info
