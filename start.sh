c7e73ac34a1cdec2868a4fab6249137978c4be9e
Saim-Webiiify <navedsaim1@gmail.com>
2025-06-24 18:16:40 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Use default port if not set
-PORT=${PORT:-10000}
-
-# Run rembg server on the correct port
-exec rembg s --host 0.0.0.0 --port $PORT --log-level info
+exec gunicorn app:app --bind 0.0.0.0:$PORT --workers 1
